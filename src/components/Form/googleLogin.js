57b1128450482642ceda6c1cb750b2214ae0d7af
Mohamed Abdelmegeed <mohamedabdelmageed1095@gmail.com>
2022-07-06 04:36:59 +0200

    final-commit

--- src/components/Form/googleLogin.js
+++ src/components/Form/googleLogin.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { GoogleLogin } from "react-google-login";
-import classes from "./form.module.css";
 
 const clientId =
   "491911339479-fcmdu5j4t2tvl58dpcbom51acnq4jsid.apps.googleusercontent.com";
@@ -19,7 +18,6 @@ const Login = () => {
       onSuccess={onSuccess}
       onFailure={onFailure}
       cookiePolicy={"single_host_origin"}
-      style={classes.google}
       isSignedIn={true}
     />
   );
